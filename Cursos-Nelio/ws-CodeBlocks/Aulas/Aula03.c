ca7b42ad66212874d4445e0329edcd8f506eeb97
Luiz Paulo Medeiros <132026611+LuizPauloJRM@users.noreply.github.com>
2024-02-24 09:29:20 -0300

    Atualizacoes em c

---
+++ Cursos-Nelio/ws-CodeBlocks/Aulas/Aula03.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+int main(){
+    printf("Bom dia\n");//quebra de linha
+    printf("Boa noite\n");
+
+}
---
+++ PycharmProjects/CursoemVideoPython/ex02.py
@@ -0,0 +1,19 @@
+nome=input('Qual é o seu nome?')
+print('Prazer em conhecer',nome)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
