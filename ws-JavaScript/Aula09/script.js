2118dafada8d1dd2527df2285c0f3cbd9420fd7b
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2024-04-02 19:22:10 -0300

    atualizando

--- ws-JavaScript/Aula09/script.js
+++ ws-JavaScript/Aula09/script.js
@@ -1,40 +1,41 @@
 function Verificador() {
-    var data = new Date();
+    var data =new Date();
     var ano = data.getFullYear();
     var fano = document.getElementById('txtano');
     var res = document.querySelector('div#res');
-    if (fano.value.length == 0 || fano.value > ano) {
+    if(fano.ariaValueMax.length == 0 ||  fano.value > ano){
         window.alert('[ERRO] Verifique todos os dados e tente novamente!');
-    } else {
+    }else{
         var fsex = document.getElementsByName('radsex');
         var idade = ano - Number(fano.value);
         var genero = '';
         var img = document.createElement('img');
         img.setAttribute('id', 'foto');
-        if (fsex[0].checked) {
+        if (fsex[0].Checked) {
             genero = 'Homem';
-            if (idade >= 0 && idade < 10) {
+            if(idade >=0 && idade< 10){
                 //criança
-            } else if (idade >= 10 && idade < 21) {
+            }else if (idade >= 10 && idade < 21){
                 //jovem
-            } else if (idade >= 21 && idade < 50) {
+            }else if (idade >= 21 && idade < 50){
                 //adulto
-            } else {
+            }else{
                 //idoso
             }
-        } else if (fsex[1].checked) {
+        }else if (fsex[1].Checked) {
             genero = 'Mulher';
-            if (idade >= 0 && idade < 10) {
+            if(idade >=0 &&  idade < 10){
                 //criança
-            } else if (idade >= 10 && idade < 21) {
+            }else if (idade >= 10 && idade < 21){
                 //jovem
-            } else if (idade >= 21 && idade < 50) {
+            }else if (idade >= 21 && idade < 50){
                 //adulto
-            } else {
+            }else{
                 //idoso
             }
         }
-        res.style.textAlign = 'center';
+        res.computedStyleMap.textAlign = 'center';
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
     }
+
 }
--- ws-Python/main2.py
+++
@@ -1,3 +0,0 @@
-idade=int(input("Qual é a  sua idade?"))
-
-if idade>=21
