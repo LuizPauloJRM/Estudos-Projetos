32bf67e1db4e5cb849697372fbbc34739d5c4384
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2024-05-17 18:28:16 -0300

    python aula 23

---
+++ Cursos-Udemy/Python3-LuizOctavio/Aula23.py
@@ -0,0 +1,17 @@
+#Fatiamento strigs
+#0123456789
+#Luiz Paulo
+#-9-8-7-6-5-4-3-2-1
+#Fatiamento [inicio:fim:passa]
+#Len -> Retorna a quantdade de caracteres da string
+variavel1='Luiz Paulo'
+variavel2='Python'
+print (variavel1[-4])
+print (variavel2[0:4])#Começa do indice 0 "P"e adiciona as demais letras até o 4 "h"
+#Contagem de caracteres 
+print(len(variavel1))
+print(variavel1[0:4:1])
+print(len(variavel1[3]))#Somente 1 indice
+print(len(variavel2))
+#[inicio:fim:passa]
+print(variavel2[0:4:2])
---
+++ Cursos-Udemy/Python3-LuizOctavio/Exercicios23.py
@@ -0,0 +1,16 @@
+"""
+Peça ao usuário para digitar seu nome, idade
+Se o nome e idade forem digitados : 
+    Exiba:
+     Seu nome é {nome}
+     Seu nome invertido é {nome invertido}
+     Se nome tem ou não espaços
+     Seu nome tem {n} letras
+     A primeira letra do seu nome é ... 
+     A última letra do seu nome é...
+     Seu nada for digitado em nome ou idade exiba "Campos vazios"
+"""
+nome = input('Digite seu nome : ')
+idade = input ('Digite sua idade : ')
+print (nome) 
+print (idade)
