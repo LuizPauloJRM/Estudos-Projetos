cb16a6877450636d63115150530d7e4db9236df6
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2024-09-16 20:28:30 -0300

    Python : Aula 36

--- Cursos-Udemy/Python3-LuizOctavio/Aula36.py
+++ Cursos-Udemy/Python3-LuizOctavio/Aula36.py
@@ -1,3 +1,35 @@
 """
 For / in - Estrutura de repetição para coisas finitas 
+For
+
 """
+"""
+   texto = 'Python'
+i=0
+tamanho_texto = len(texto)
+while i < tamanho_texto:
+    print(texto[i], i)
+    i+=1 
+"""
+
+"""
+Exemplo de quando não sabe precisamente quantas repetições vou ter 
+senha_salva = '12345'
+senha_digitada=''
+repeticoes=0
+while senha_salva != senha_digitada:
+    senha_digitada = input(f'Senha({repeticoes}x): ')
+    repeticoes+=1
+print(repeticoes)
+print('Laço com repetições infinitas')
+
+"""
+
+#Exemplo For (Para), iteravel passando em cada uma com uma iteração , sabe o tamanho que vai se repetir 
+texto = 'Python'
+novo_texto=''
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
+
