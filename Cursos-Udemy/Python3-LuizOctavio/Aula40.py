cdb929719645f03fcdc52dfc47c057e041fe473c
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2024-09-23 22:50:18 -0300

    funcoes javaScript bind e Arrow

--- Cursos-Udemy/Python3-LuizOctavio/Aula40.py
+++ Cursos-Udemy/Python3-LuizOctavio/Aula40.py
@@ -5,7 +5,28 @@ Iterador -> Um valor por vez
 next -> próximo valor 
 iter -> seu iterador 
 """
+texto = iter('Luiz')#__iter__()
+"""
+print(texto,__next__())
+"""
+print(next(texto))
+print(next(texto))
+print(next(texto))
+print(next(texto))
+#print(next(texto)) Sem esgotar os valores 
+
+print (texto)
+#texto = 'Luiz' Iter ()
 texto = "Luiz"
 numeros = range (0,100,8)
 for numero in numeros:
     print(numeros)
+print('')
+#For letra in text solicitando para o objeto o iterador dele 
+text='Paulo'#Iteravel
+iterador = iter(texto)#iterator
+
+while True:
+    try:
+        print(next(iterador))
+    except StopIteration:
